db1cd4defa55ce5aab1ddb0b285197c7552f4205
Ashlok Chaudhary <chaudharyashlok@gmail.com>
2025-04-21 15:13:30 +0530

    Align with schema && Dockerfiles Creation

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,7 @@ const router = Router();
 router.use('/templates', templatesRoutes);
 router.use('/attachment', attachmentRoutes);
 router.use('/leads', leadsRoutes);
-router.use('/message', messageRoutes);
-router.use('/promotion', promotionRoutes);
+router.use('/messages', messageRoutes);
+router.use('/promotions', promotionRoutes);
 
 export default router;
